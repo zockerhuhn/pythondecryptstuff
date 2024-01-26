42d0a7799032c292e21d06a0a5d0c80d26c037e8
zockerhuhn <87993969+zockerhuhn@users.noreply.github.com>
2024-01-26 09:58:05 +0000

    Not working

--- main.py
+++ main.py
@@ -39,6 +39,7 @@ def xyz(original):
   distribution = [17.40,9.78,7.55,7.27,7.00,6.51,6.15,5.08,4.76,4.35,3.44,3.06,3.01,2.53,2.51,1.89,1.89,1.66,1.21,1.13,0.79,0.67,0.27,0.04,0.03,0.02]
   sortedletters = ["e","n","i","s","r","a","t","d","h","u","l","c","g","m","o","b","w","f","k","z","p","v","j","y","x","q"]
   origdistribution = {}
+  overwritekey = {}
   bestscore = int(1) #set this to minimun amount of hits to count as valid
   best = ""
   for i in letters:
@@ -47,6 +48,7 @@ def xyz(original):
       if x == i:
         hits += 1
     origdistribution[i]=hits
+    overwritekey[i]=None
   origdistribution = dict(sorted(origdistribution.items(), key=lambda item: item[1], reverse=True))
   print(origdistribution)
   key = []
@@ -57,11 +59,19 @@ def xyz(original):
     #print(key)
     for i in original:
       if i in letters:
-        tempresult += sortedletters[get_nth_key(origdistribution,i)+minmax(key[letters.index(i)]-1,int(0),int(25))]
+        if overwritekey[i] is None:
+          tempresult += sortedletters[get_nth_key(origdistribution,i)+minmax(key[letters.index(i)]-1,int(0),int(25))]
+        else:
+          tempresult += overwritekey[i]
+    temp=[]
     temp=compare(tempresult,best,bestscore)
-    best = temp[0]
+    best = str(temp[0])
     bestscore = temp[1]
     print(tempresult)
+    if temp[2] is not None:
+      for char in temp[2]:
+        overwritekey[char]= sortedletters[get_nth_key(origdistribution,char)+minmax(key[letters.index(char)]-1,int(0),int(25))]
+    print(overwritekey)
     key[0]+=1
     try:
       while key.index(3) is not None:
@@ -84,23 +94,23 @@ def minmax(value, minimum, maximum):
 def checktext(texttocheck):
   loop = 0
   amount = 0
-  for splitted in texttocheck:
-    amount = 0
-    alreadyhit = []
-    for word in listofwords:
-      loop += 1
-      if len(word) >= 3:
-        if word in splitted:
-          if word not in alreadyhit:
-            amount += 1
-            #print(word)
-            alreadyhit.append(word)
+  alreadyhit = []
+  for word in listofwords:
+    loop += 1
+    if len(word) >= 3:
+      if word in texttocheck:
+        if word not in alreadyhit:
+          amount += 1
+          #print(word)
+          alreadyhit.append(word)
     #print(loop)
-    treffer.append(amount)
-  return amount
+  treffer.append(amount)
+  return [amount,alreadyhit]
 
 def compare(tempresult,best,bestscore):
-  score = checktext(tempresult)
+  temp = checktext(tempresult)[0]
+  score = int(temp[0])
+  words = temp[1]
   if score == bestscore:
     best += '\n' + tempresult
     print("added %s with %s matches" % (tempresult,score))
@@ -108,7 +118,7 @@ def compare(tempresult,best,bestscore):
     best = tempresult
     bestscore = score
     print("replaced %s with %s with %s matches" % (best,tempresult,score))
-  return [best,bestscore]
+  return [best,bestscore,words]
 
 def gartenzaun_en(text):
   return text[::2] + text[1::2]
@@ -128,7 +138,8 @@ def gartenzaun_de(text):
 if input("caeser/gartenzaun [c/g]") == "c":
   caeserdecrypt(message, 1)
   splittedtext = walloftext.split(';')
-  checktext(splittedtext)
+  for i in splittedtext:
+    checktext(i)
   #print(splittedtext)
   print("most matches (%s) in word: %s with key: %s/%s" % (max(treffer) ,splittedtext[(treffer.index(max(treffer)))], treffer.index(max(treffer))+1, treffer.index(max(treffer))-25))
   #print(treffer)
